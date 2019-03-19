2760de072bc3cd5c0dbae2007b41913d325db44f
zhouxiang19910319 <zhouxiang19910319@gmail.com>
2019-03-19 17:45:19 +0800

    .

--- src/components/bio.js
+++ src/components/bio.js
@@ -2,8 +2,6 @@ import React from "react"
 import Layout from "./layout"
 // import SEO from "./seo"
 import profile_pic from "../images/profile_pic.png"
-// import { Component, Fragment } from 'react'
-// import { constants } from "os"
 // import 'bootstrap/dist/css/bootstrap.css'
 
 class Bio extends React.Component{
