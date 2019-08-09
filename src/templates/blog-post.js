21297e8957a0027c961b98c36fa10a6e556d76c2
zhouxiang19910319 <zhouxiang19910319@gmail.com>
2019-08-09 22:01:32 +0800

    .

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,8 +5,7 @@ import Layout from '../components/layout'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import Container from 'react-bootstrap/Container'
-// import Row from 'react-bootstrap/Row'
-// import Col from 'react-bootstrap/Col'
+
 
 
 
